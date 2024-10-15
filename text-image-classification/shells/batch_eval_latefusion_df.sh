9859285e0ef7695c41fb7ecf20d462c95b8dc97d
qingyang zhang <52411664+QingyangZhang@users.noreply.github.com>
2024-10-15 12:53:44 +0800

    Update batch_eval_latefusion_qmf.sh

--- text-image-classification/shells/batch_eval_latefusion_df.sh
+++ text-image-classification/shells/batch_eval_latefusion_df.sh
@@ -10,7 +10,7 @@ do
       model=latefusion
       name=$task"_"$model"_model_run_df_$i"
       echo $name noise: $noise seed: $seed
-      python eval_df.py --batch_sz 16 --gradient_accumulation_steps 40  \
+      python eval_qmf.py --batch_sz 16 --gradient_accumulation_steps 40  \
       --savedir ./saved/$task --name $name  --data_path ./datasets/ \
        --task $task --task_type $task_type  --model $model --num_image_embeds 3 \
        --freeze_txt 5 --freeze_img 3   --patience 5 --dropout 0.1 --lr 5e-05 --warmup 0.1 --max_epochs 100 --seed $seed --df true --noise $noise
