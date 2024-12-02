90bf6b1871fdcdbb93dce51bbdc28599dbe6cca3
Hai-Tao Wu <61724827+HaitaoWuTJU@users.noreply.github.com>
2024-12-02 20:20:18 +0800

    Update batch_eval_latefusion_qmf.sh

--- text-image-classification/shells/batch_eval_latefusion_qmf.sh
+++ text-image-classification/shells/batch_eval_latefusion_qmf.sh
@@ -13,7 +13,7 @@ do
       python eval_qmf.py --batch_sz 16 --gradient_accumulation_steps 40  \
       --savedir ./saved/$task --name $name  --data_path ./datasets/ \
        --task $task --task_type $task_type  --model $model --num_image_embeds 3 \
-       --freeze_txt 5 --freeze_img 3   --patience 5 --dropout 0.1 --lr 5e-05 --warmup 0.1 --max_epochs 100 --seed $seed --df true --noise $noise
+       --freeze_txt 5 --freeze_img 3   --patience 5 --dropout 0.1 --lr 5e-05 --warmup 0.1 --max_epochs 100 --seed $seed --df true --noise $noise --noise_type Gaussian #Gaussian Salt
     done
   done
 done
