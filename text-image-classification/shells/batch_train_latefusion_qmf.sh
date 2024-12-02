1da541a850e023520f7617c752dc99904e46d15f
Hai-Tao Wu <61724827+HaitaoWuTJU@users.noreply.github.com>
2024-12-02 20:14:16 +0800

    Update batch_train_latefusion_qmf.sh

--- text-image-classification/shells/batch_train_latefusion_qmf.sh
+++ text-image-classification/shells/batch_train_latefusion_qmf.sh
@@ -4,7 +4,7 @@ do
   task="MVSA_Single"
   task_type="classification"
   model="latefusion"
-  name=$task"_"$model"_model_run_df_$i"
+  name=$task"_"$model"_model_run_qmf_$i"
   echo $name seed: $i
   CUDA_VISIBLE_DEVICES=7  python train_qmf.py --batch_sz 16 --gradient_accumulation_steps 40  \
     --savedir ./saved/$task --name $name  --data_path ./datasets/ \
